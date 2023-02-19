e2793248100d384630ad8dca67891aa2af4b2f9a
Nico Gallinal <nico@rematter.com>
2023-02-19 16:57:20 -0300

    update gitignore

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { QueryInterface } from 'sequelize';
-declare type Options = {
-    getPrimaryKey?: (primaryTable: string) => string;
-};
-export declare const queryInterfaceDecorator: (queryInterface: QueryInterface, options?: Options | undefined) => QueryInterface;
-export {};
