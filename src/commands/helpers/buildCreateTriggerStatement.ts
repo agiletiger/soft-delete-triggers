2257a8c062dfa2f85747ad21f1727919a588ad66
Nico Gallinal <nico@rematter.com>
2023-02-19 16:21:25 -0300

    escape table and column names when creating trigger, try keeping trigger name under 63 chars

--- src/commands/helpers/buildCreateTriggerStatement.ts
+++ src/commands/helpers/buildCreateTriggerStatement.ts
@@ -8,12 +8,12 @@ export const buildCreateTriggerStatement = (
 ): string => /* sql */ `
   CREATE TRIGGER ${buildTriggerName(primaryTable, foreignTable)}
   AFTER UPDATE
-  ON ${primaryTable} FOR EACH ROW
+  ON \`${primaryTable}\` FOR EACH ROW
   BEGIN
-    IF OLD.deletedAt IS NULL AND NEW.deletedAt IS NOT NULL THEN
-      UPDATE ${foreignTable}
-        SET ${foreignTable}.deletedAt = NOW()
-      WHERE ${foreignTable}.${foreignKey} = NEW.${primaryKey};
+    IF \`OLD\`.\`deletedAt\` IS NULL AND \`NEW\`.\`deletedAt\` IS NOT NULL THEN
+      UPDATE \`${foreignTable}\`
+        SET \`${foreignTable}\`.\`deletedAt\` = NOW()
+      WHERE \`${foreignTable}\`.\`${foreignKey}\` = \`NEW\`.\`${primaryKey}\`;
     END IF;
   END;
 `;
--- src/commands/helpers/buildTriggerName.ts
+++ src/commands/helpers/buildTriggerName.ts
@@ -1,2 +1,14 @@
+// MYSQL has a limit of 63 chars on triggers name
+// if table 'big' we remove the vowels hoping the trigger name will fit in 63 chars
+const MAX_TABLE_NAME_LENGTH = 16;
+
 export const buildTriggerName = (primaryTable: string, foreignTable: string): string =>
-  `on_${primaryTable}_delete_update_${foreignTable}`;
+  `on_${
+    primaryTable.length > MAX_TABLE_NAME_LENGTH
+      ? primaryTable.replace(/[aeiou]/gi, '')
+      : primaryTable
+  }_del_upd_${
+    foreignTable.length > MAX_TABLE_NAME_LENGTH
+      ? foreignTable.replace(/[aeiou]/gi, '')
+      : foreignTable
+  }`;
